3fd377c1bc8693ca4637a86478b85405ed164811
seemantshekhar43 <seemantshekhar43@gmail.com>
2026-07-18 20:26:37 +0530

    added nix home-manager herdr nvim weztern homebrew

---
+++ dotfiles/home/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'NeogitOrg/neogit',
+    dependencies = { 'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim' },
+    keys = { { '<leader>g', function() require('neogit').open() end, desc = 'Neogit' } },
+  },
+  {
+    'lewis6999/gitsigns.nvim',
+    event = 'BufWinEnter',
+    opts = { current_line_blame = true },  -- who last touched this line
+  },
+}
+
+
+
+
